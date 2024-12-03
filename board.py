50b03f6650c898284c24a25c3eea7c8d8747c764
Patrick Ugwu <patrick.ugwu@web.de>
2024-12-03 10:00:46 +0100

    visual adjustments to board

--- board.py
+++ board.py
@@ -16,18 +16,18 @@ class Board:
         # divide board into 9 cells
         outer_cell_line_width = 10
         for i in range(1,3):
-            p.draw.rect(self.screen, "black", p.Rect(0, (i*self.SCREEN_HEIGHT/3) - (outer_cell_line_width / 2), self.SCREEN_WIDTH, outer_cell_line_width))
+            p.draw.rect(self.screen, "black", p.Rect(0, (i*self.SCREEN_HEIGHT/3) - (outer_cell_line_width), self.SCREEN_WIDTH, outer_cell_line_width))
             p.draw.rect(self.screen, "black", p.Rect((i*self.SCREEN_WIDTH/3) - (outer_cell_line_width / 2), 0, outer_cell_line_width, self.SCREEN_HEIGHT))
         
         # divide each cell into 9 squares
         inner_cell_line_width = 5
         for i in range(1,9):
-            p.draw.rect(self.screen, "black", p.Rect(0, (i*self.SCREEN_HEIGHT/9) - (inner_cell_line_width / 2), self.SCREEN_WIDTH, inner_cell_line_width))
+            p.draw.rect(self.screen, "black", p.Rect(0, (i*self.SCREEN_HEIGHT/9) - (inner_cell_line_width), self.SCREEN_WIDTH, inner_cell_line_width))
             p.draw.rect(self.screen, "black", p.Rect((i*self.SCREEN_WIDTH/9) - (inner_cell_line_width / 2), 0, inner_cell_line_width, self.SCREEN_HEIGHT))
 
     def draw_number(self,num, row, col):
         # set up number
-        number_size = round(self.SCREEN_HEIGHT/18)
+        number_size = round(self.SCREEN_HEIGHT/9)
         font = p.font.Font(None, number_size)
         number = font.render(str(num), True, (0, 0, 0)) 
         
