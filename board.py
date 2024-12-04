b5ca6a499a127f7e2e882e748a3cc5adeebef6e9
Patrick Ugwu <patrick.ugwu@web.de>
2024-12-04 19:43:20 +0100

    added Solver class

--- board.py
+++ board.py
@@ -25,15 +25,15 @@ class Board:
             p.draw.rect(self.screen, "black", p.Rect(0, (i*self.SCREEN_HEIGHT/9) - (inner_cell_line_width), self.SCREEN_WIDTH, inner_cell_line_width))
             p.draw.rect(self.screen, "black", p.Rect((i*self.SCREEN_WIDTH/9) - (inner_cell_line_width / 2), 0, inner_cell_line_width, self.SCREEN_HEIGHT))
 
-    def draw_number(self,num, row, col):
+    def draw_number(self, row, col, num):
         # set up number
         number_size = round(self.SCREEN_HEIGHT/9)
         font = p.font.Font(None, number_size)
         number = font.render(str(num), True, (0, 0, 0)) 
         
         # center number into correct square
-        width = (col+1) * self.SCREEN_WIDTH/9 - self.SCREEN_WIDTH/18 - number.get_size()[0]/2
-        height = (row+1) * self.SCREEN_HEIGHT/9 - self.SCREEN_HEIGHT/18 - number.get_size()[1]/2
+        width = (col + 1) * self.SCREEN_WIDTH/9 - self.SCREEN_WIDTH/18 - number.get_size()[0]/2
+        height = (row + 1) * self.SCREEN_HEIGHT/9 - self.SCREEN_HEIGHT/18 - number.get_size()[1]/2
         self.screen.blit(number, (width, height))
 
 
---
+++ solver.py
@@ -0,0 +1,48 @@
+class Solver:
+    NUMBERS = [i for i in range(1,10)]
+
+    def __init__(self,gameboard):
+        self.board = gameboard.board
+        print(self.NUMBERS)
+    
+    def find_empty_square(self):
+        for row in range(9):
+            for col in range(9):
+                if self.board[row][col] == 0:
+                    return row, col
+    
+    def is_valid(self,row, col, number):
+        # check row
+        for val in self.board[row]:
+            if val == number:
+                return False  
+
+        # check col    
+        for val in range(9):
+            if self.board[val][col] == number:
+                return False 
+        
+        #check cell
+        row_start = (row // 3) * 3
+        col_start = (col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == number:
+                    return False
+              
+        return True
+    
+
+    def solve(self):
+        if (empty_square := self.find_empty_square()) is None:
+            return True
+        else:
+            for number in self.NUMBERS:
+                if self.is_valid(empty_square[0], empty_square[1], number):
+                    self.board[empty_square[0]][empty_square[1]] = number
+                    if self.solve():
+                        return True
+                    else:
+                        self.board[empty_square[0]][empty_square[1]] = 0
+
+            return False
