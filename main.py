0af2265756a01cbb09efeacc00f588dfc2a5bdf1
Patrick Ugwu <patrick.ugwu@web.de>
2024-12-05 14:00:56 +0100

    draw solution for example sudoku

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import board, pygame, random
+import board, pygame, random, solver
 
 # random board
-puzzle = [[random.randint(1,9) for _ in range(9)] for _ in range(9)]
+#puzzle = [[random.randint(1,9) for _ in range(9)] for _ in range(9)]
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
 
 sudoku = board.Board(puzzle)
-print(puzzle)
+solved_sudoku = solver.Solver(sudoku)
+solved_sudoku.solve()
+print(solved_sudoku.board)
+
 
 running = True
 while running:
@@ -13,12 +28,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        
     sudoku.draw_grid()
     
-    # fill in example numbers
-    sudoku.draw_number(3,1,1)
-    sudoku.draw_number(1,3,5)
+  
+    for row in range(9):
+      for col in range(9):
+        sudoku.draw_number(row, col, solved_sudoku.board[row][col])
 
     # update screen
     pygame.display.flip()
